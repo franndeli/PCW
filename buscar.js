44e35b96b4a53f9ce481925548a9908e731c925d
Frandeli <124245758+franndeli@users.noreply.github.com>
2023-04-30 20:09:17 +0200

    FINAL?

--- buscar.js
+++ buscar.js
@@ -3,9 +3,9 @@
 document.getElementById("formulario").addEventListener("submit", function(event) {
     event.preventDefault();
     buscar();
-  });
+});
 
-  function mostrarResultados(publicaciones) {
+function mostrarResultados(publicaciones, totalPublicaciones, pagina) {
     let html = '';
     publicaciones.forEach(function(e) {
         const fecha = new Date(e.fechaCreacion);
@@ -18,84 +18,91 @@ document.getElementById("formulario").addEventListener("submit", function(event)
             </li>`;
     });
     document.querySelector('#publi').innerHTML = html;
+    
+    const totalPaginas = Math.ceil(totalPublicaciones / 6);
+    const paginationElement = document.getElementById('pagination');
+    
+    if (totalPublicaciones > 0) {
+        paginationElement.style.display = 'block';
+        document.querySelector('#current').innerHTML = `Página ${pagina + 1} de ${totalPaginas}`;
+    } else {
+        paginationElement.style.display = 'none';
     }
-  
-  function buscar() {
-    // Obtener la información de los campos del formulario
-    let nombreBuscar = document.getElementById("nombrebuscar").value;
-    let fechaInicio = document.getElementById("start").value;
-    let fechaFin = document.getElementById("end").value;
-    let ubicacion = document.getElementById("nivel").value;
-  
-    // Realizar la búsqueda con la información obtenida
-    buscarPublicaciones(nombreBuscar, fechaInicio, fechaFin, ubicacion);
-  }
-  
-  function buscarPublicaciones(nombreBuscar, fechaInicio, fechaFin, ubicacion) {
-    // Ejemplo de cómo mostrar las publicaciones filtradas en el elemento con id "publi"
-    let publi = document.getElementById("publi");
-    let publicacionesFiltradas = []; // Aquí se deben almacenar las publicaciones filtradas según los criterios de búsqueda
-  
-    publicacionesFiltradas.forEach((publicacion) => {
-      let listItem = document.createElement("li");
-      listItem.innerHTML = publicacion; // Aquí debes adaptar el código para mostrar la información de cada publicación de manera adecuada
-      publi.appendChild(listItem);
-    });
-  }
+}
 
-    function buscarPorUrl() {
-        const urlParams = new URLSearchParams(window.location.search);
-        const nombreZona = urlParams.get("nombreZona");
-        if (nombreZona) {
-            document.getElementById("nivel").value = nombreZona;
-            buscar();
-        }
+function buscar(pagina = 0) {
+    let texto = document.getElementById("nombrebuscar").value;
+    let fechaDesde = document.getElementById("start").value;
+    let fechaHasta = document.getElementById("end").value;
+    let zona = document.getElementById("nivel").value;
+    let registrosPorPagina = 6;
+
+    let url = "api/publicaciones?";
+    let parametros = [];
+
+    if (texto) {
+        parametros.push("t=" + encodeURIComponent(texto));
     }
-
-    function buscar() {
-        let texto = document.getElementById("nombrebuscar").value;
-        let fechaDesde = document.getElementById("start").value;
-        let fechaHasta = document.getElementById("end").value;
-        let zona = document.getElementById("nivel").value;
-        let pagina = 0;
-        let registrosPorPagina = 6;
-
-        let url = "api/publicaciones?";
-        let parametros = [];
-
-        if (texto) {
-            parametros.push("t=" + encodeURIComponent(texto));
-        }
-        if (fechaDesde) {
-            parametros.push("fd=" + encodeURIComponent(fechaDesde));
-        }
-        if (fechaHasta) {
-            parametros.push("fh=" + encodeURIComponent(fechaHasta));
-        }
-        if (zona) {
-            parametros.push("z=" + encodeURIComponent(zona));
-        }
-        parametros.push("pag=" + encodeURIComponent(pagina));
-        parametros.push("lpag=" + encodeURIComponent(registrosPorPagina));
-
-        url += parametros.join("&");
-
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Error en la petición: " + response.status);
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.RESULTADO === "OK") {
-                    mostrarResultados(data.FILAS);
-                }
-            })
-            .catch(error => {
-                console.error("Error al realizar la búsqueda: ", error);
-            });
+    if (fechaDesde) {
+        parametros.push("fd=" + encodeURIComponent(fechaDesde));
     }
+    if (fechaHasta) {
+        parametros.push("fh=" + encodeURIComponent(fechaHasta));
+    }
+    if (zona) {
+        parametros.push("z=" + encodeURIComponent(zona));
+    }
+    parametros.push("pag=" + encodeURIComponent(pagina));
+    parametros.push("lpag=" + encodeURIComponent(registrosPorPagina));
+
+    url += parametros.join("&");
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error en la petición: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.RESULTADO === "OK") {
+                mostrarResultados(data.FILAS, data.TOTAL_COINCIDENCIAS, pagina);
+                actualizarBotones(data.TOTAL_COINCIDENCIAS, pagina);
+            }
+        })
+        .catch(error => {
+            console.error("Error al realizar la búsqueda: ", error);
+        });
+}
+
+function buscarPorUrl() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const nombreZona = urlParams.get("nombreZona");
+    if (nombreZona) {
+        document.getElementById("nivel").value = nombreZona;
+        buscar();
+    }
+}
+
+function actualizarBotones(totalPublicaciones, paginaActual) {
+    const totalPaginas = Math.ceil(totalPublicaciones / 6);
+    document.querySelector('#first').disabled = paginaActual === 0;
+    document.querySelector('#previous').disabled = paginaActual === 0;
+    document.querySelector('#next').disabled = paginaActual === (totalPaginas - 1);
+    document.querySelector('#last').disabled = paginaActual === (totalPaginas - 1);
+
+    document.querySelector('#first').onclick = function() {
+        buscar(0);
+    };
+    document.querySelector('#previous').onclick = function() {
+        buscar(paginaActual - 1);
+    };
+    document.querySelector('#next').onclick = function() {
+        buscar(paginaActual + 1);
+    };
+    document.querySelector('#last').onclick = function() {
+        buscar(totalPaginas - 1);
+    };
+}
 
 window.addEventListener('DOMContentLoaded', buscarPorUrl);
-
--- index.js
+++ index.js
@@ -1,64 +1,35 @@
 'use strict'
-let currentPage = 1;
 
-function mostrarFotos(pagina, inicio, fin) { 
-    if (pagina) {
-        currentPage = pagina;
-    }
-    if(pagina == 1){
-        inicio = 0; fin = 5;
-    }
-    if (inicio < 0) {
-        inicio = 0;
-        fin = 5;
-    }
-    if (pagina < 1) {
-        pagina = 1;
-    }
-    if (fin < inicio) {
-        fin = inicio + 5;
-    }
-    let son = [];
+function mostrarFotos(pagina) {
+    const registrosPorPagina = 6;
     let xhr = new XMLHttpRequest(),
-    url = `api/publicaciones?pag=${currentPage}`;
+        url = `api/publicaciones?pag=${pagina}&lpag=${registrosPorPagina}`;
     xhr.open('GET', url, true);
     xhr.onload = function() {
         let respuesta = JSON.parse(xhr.responseText),
             html = '';
-        
+
         respuesta.FILAS.forEach(function(e) {
-          son.push(e);
+            const fecha = new Date(e.fechaCreacion);
+            const fechaString = `${('0' + fecha.getDate()).slice(-2)}-${('0' + (fecha.getMonth() + 1)).slice(-2)}-${fecha.getFullYear()} `;
+            html += `<li><a href="publicacion.html?id=${e.id}" title="${e.titulo}">
+                <div class="imagen"><img src="fotos/pubs/${e.imagen}" width="280" height="210" alt="${e.titulo}"></div>
+                <h3>${e.titulo}</h3></a>
+                <p>${e.autor}</p>
+                <p>${fechaString}</p>
+                </li>`;
         });
-        let contador = 0;
-        let longitud = son.length; 
-        if(fin > longitud){
-            fin = longitud - 1;
-        }
-        if(contador < 6){
-            for(let i = inicio; i<=fin; i++) {
-                if (son[i]) {
-                    const fecha = new Date(son[i].fechaCreacion);
-                    const fechaString = `${('0' + fecha.getDate()).slice(-2)}-${('0' + (fecha.getMonth() + 1)).slice(-2)}-${fecha.getFullYear()} `;
-                    //console.log(i);
-                    //console.log(son[i]);
-                    html += `<li><a href="publicacion.html?id=${son[i].id}" title="${son[i].titulo}">
-                        <div class="imagen"><img src="fotos/pubs/${son[i].imagen}" width="280" height="210" alt="${son[i].titulo}"></div>
-                        <h3>${son[i].titulo}</h3></a>
-                        <p>${son[i].autor}</p>
-                        <p>${fechaString}</p>
-                        </li>`;
-                }
-            }
-            contador++;
-        }        
+
         document.querySelector('#publi').innerHTML = html;
-        // calcular la cantidad de páginas necesarias
-        let totalPublicaciones = respuesta.FILAS.length;
-        let cantidadPaginas = Math.ceil(totalPublicaciones / 6);
+
+        // Convertir los valores de PAG y LPAG a números
+        const paginaActual = parseInt(respuesta.PAG, 10);
+        const totalPublicaciones = parseInt(respuesta.TOTAL_COINCIDENCIAS, 10);
+        const totalPaginas = Math.ceil(totalPublicaciones / registrosPorPagina);
 
         // actualizar el texto de la barra de navegación
         let currentElement = document.querySelector('#current');
-        currentElement.innerHTML = `Página ${pagina} de ${cantidadPaginas}`;
+        currentElement.innerHTML = `Página ${paginaActual + 1} de ${totalPaginas}`;
 
         // desactivar o activar los botones de navegación según corresponda
         let firstButton = document.querySelector('#first');
@@ -66,44 +37,34 @@ function mostrarFotos(pagina, inicio, fin) {
         let nextButton = document.querySelector('#next');
         let lastButton = document.querySelector('#last');
 
-        if (pagina === 1) {
-            firstButton.disabled = true;
-            previousButton.disabled = true;
-        } else {
-            firstButton.disabled = false;
-            previousButton.disabled = false;
-        }
-
-        if (pagina === cantidadPaginas) {
-            nextButton.disabled = true;
-            lastButton.disabled = true;
-        } else {
-            nextButton.disabled = false;
-            lastButton.disabled = false;
-        }
+        firstButton.disabled = paginaActual === 0;
+        previousButton.disabled = paginaActual === 0;
+        nextButton.disabled = paginaActual === (totalPaginas - 1);
+        lastButton.disabled = paginaActual === (totalPaginas - 1);
 
         // agregar los event listeners a los botones de navegación
-        firstButton.addEventListener('click', function() {
-            inicio=0;
-            fin=5;
-            mostrarFotos(1, inicio);
-        });
-        previousButton.addEventListener('click', function() {
-            mostrarFotos(pagina - 1, inicio - 5, fin - 5);
-        });
-        nextButton.addEventListener('click', function() {
-            mostrarFotos(pagina + 1, inicio + 6, fin + 6);
-        });
-        lastButton.addEventListener('click', function() {
-            let ultimaPagina = cantidadPaginas;
-            let inicioUltimaPagina = (ultimaPagina - 1) * 6;
-            let finUltimaPagina = totalPublicaciones - 1;
-            mostrarFotos(ultimaPagina, inicioUltimaPagina, finUltimaPagina);
-          });
+        firstButton.onclick = function() {
+            mostrarFotos(0);
+        };
+        previousButton.onclick = function() {
+            mostrarFotos(paginaActual - 1);
+        };
+        nextButton.onclick = function() {
+            mostrarFotos(paginaActual + 1);
+        };
+        lastButton.onclick = function() {
+            mostrarFotos(totalPaginas - 1);
+        };
     };
 
     xhr.send();
 }
+
+// Llama a la función al cargar la página para mostrar las primeras 6 publicaciones
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarFotos(0);
+});
+
 /*
 function mostrarFotos(){
     let xhr = new XMLHttpRequest(),
